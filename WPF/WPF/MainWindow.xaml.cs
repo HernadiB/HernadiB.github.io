7b26eaa82c4a9b61cde4908ec32341cda8fc0b14
Roli37 <nyari.roland.david@njit.hu>
2021-12-30 14:30:43 +0100

    WPF

---
+++ WPF/WPF/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF.Models;
+using static WPF.Models.MainWindowViewModel;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_megjelenit_Click(object sender, RoutedEventArgs e)
+        {
+            Megjelenit(AllData(OpenConnection()));
+        }
+
+        private void btn_exportal_Click(object sender, RoutedEventArgs e)
+        {
+            Exportal(AllData(OpenConnection()));
+        }
+
+        private void btn_szures_Click(object sender, RoutedEventArgs e)
+        {
+            Megjelenit(FilteredData());
+        }
+
+        private void btn_ujadat_Click(object sender, RoutedEventArgs e)
+        {
+            SecondWindow secondWindow = new SecondWindow();
+            secondWindow.Show();
+        }
+        public void Megjelenit(List<Adat> data)
+        {
+            dataGrid1.ItemsSource = data;
+            dataGrid1.Visibility = Visibility.Visible;
+        }
+        public List<Adat> FilteredData() //Javítani: Ha üresek a valuek, dobjon messaget
+        {
+            string nev = tb_nev.Text;
+            int ar = int.Parse(tb_ar.Text);
+            List<Adat> lista = FilterData(AllData(OpenConnection()), nev, ar);
+            return lista;
+        }
+        public void Figyelmeztet()
+        {
+            //Implementálni
+        }
+    }
+}
---
+++ WPF/WPF/SecondWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static WPF.Models.SecondWindowViewModel;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+        public SecondWindow()
+        {
+            InitializeComponent();
+            ComboBoxes(Szerelonevek(OpenConnection()), Megrendelonevek(OpenConnection()), Szamok());
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            Insert();
+        }
+        public void Insert() //Javítani: Ha üresek a valuek, dobjon messaget
+        {
+            string szerelonev = cb1.SelectedItem.ToString();
+            string megrendelonev = cb2.SelectedItem.ToString();
+            DateTime date = calendar1.SelectedDate.Value;
+            int javido = int.Parse(cb3.SelectedItem.ToString());
+            Command(OpenConnection(), szerelonev, megrendelonev, date, javido);
+        }
+        public void ComboBoxes(List<string> szerelonevek, List<string> megrendelonevek, List<int> szamok)
+        {
+            cb1.ItemsSource = szerelonevek;
+            cb2.ItemsSource = megrendelonevek;
+            cb3.ItemsSource = szamok;
+        }
+
+    }
+}
